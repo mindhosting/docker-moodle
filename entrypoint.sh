fcd2ad923b7f58f1876d57419154faf4dd064297
sakly ayoub <saklyayoub@live.com>
2020-05-21 13:32:40 +0100

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ logo_print(){
     ██║ ╚═╝ ██║██║██║ ╚████║██████╔╝    ██║  ██║╚██████╔╝███████║   ██║   ██║██║ ╚████║╚██████╔╝
     ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝ 
                                                                                          PHP 7.3
-    MOOLDE CONTAINER (R) MARS2020 V0.7
+    MOOLDE CONTAINER (R) APRIL2020 V1.0
     FOR MIND HOSTING
-    https://mind.mindhosting
+    http://mind.mindhosting
     by SAKLY Ayoub
     saklyayoub@gmail.com
 
@@ -20,72 +20,61 @@ EOF
 }
 apache_set_servername(){
     echo "ServerName "$VIRTUAL_HOST >> /etc/apache2/apache2.conf
-    echo "[OK] APACHE SERVER NAME CONFIGURED"
 }
-moodle_setup_files(){
-    if [[ -z "$(ls -A /web_data/public_html)" ]]; then
-        cp -ar /web_data/moodle/. /web_data/public_html
-        cp /web_data/public_html/config-dist.php /web_data/public_html/config.php
-        echo "[OK] WEB DIRECTORY POPULATED WITH MOODLE FILES"
-    fi    
-}
-moodle_update_config(){
-    sed -i "/->dbtype/c\$CFG->dbtype    = 'mysqli';" /web_data/public_html/config.php
-    sed -i "/->dbhost/c\$CFG->dbhost    = 'db';" /web_data/public_html/config.php
-    sed -i "/->dbname/c\$CFG->dbname    = '"$ADMIN_USERNAME"';" /web_data/public_html/config.php
-    sed -i "/->dbuser/c\$CFG->dbuser    = '"$ADMIN_USERNAME"';" /web_data/public_html/config.php
-    sed -i "/->dbpass/c\$CFG->dbpass    = '"$ADMIN_PASSWORD"';" /web_data/public_html/config.php
-    sed -i "/->prefix/c\$CFG->prefix    = 'mdl_';" /web_data/public_html/config.php
-    sed -i "/->wwwroot/c\$CFG->wwwroot   = 'https://"$VIRTUAL_HOST"';" /web_data/public_html/config.php
-    sed -i "/->dataroot/c\$CFG->dataroot  = '/web_data/moodledata';" /web_data/public_html/config.php
-    sed -i "/->directorypermissions/c\$CFG->directorypermissions = "02777";" /web_data/public_html/config.php
-    sed -i "/->admin/c\$CFG->admin = 'admin';" /web_data/public_html/config.php
-    echo "[OK] MOODLE CONFIG FILE UPDATED"
-}
-moodle_securing_web(){
-    chown -R www-data:www-data /web_data/public_html
-    chmod -R 0755 /web_data/public_html
-    find /web_data/public_html -type f -exec chmod 0644 {} \;
-    #chmod -R +a "www-data allow read,delete,write,append,file_inherit,directory_inherit" /web_data/public_html
-    echo "[OK] MOODLE WEB SECURED"
-}
-moodle_securing_data(){
-    chmod -R 0777 /web_data/moodledata
-    echo "[OK] MOODLE DATA SECURED"
-}
-cron_service_start(){
-    service cron start 1>/dev/null 2>&1;
-    echo "[OK] CRON SERVICE STARTED"
-}
-moodle_setup_database(){
-    sleep 20
-    if [[ -f /web_data/db_data/$ADMIN_USERNAME/mdl_user.frm ]] && [[ -f /web_data/db_data/$ADMIN_USERNAME/mdl_user.ibd ]]; then
-        echo "[CHECK] MOODLE DATABASE ALRADY INITILIZED"
-    else
-        php /web_data/public_html/admin/cli/install_database.php --lang=fr --adminuser=$ADMIN_USERNAME --adminpass=$ADMIN_PASSWORD --adminemail=$ADMIN_EMAIL --agree-license --fullname=$VIRTUAL_HOST --shortname=MOODLE3.8
-        echo "[OK] MOODLE DATABASE SETTED UP"
+moodle_install(){
+    if [[ -z "$(ls -A /var/www/html)" ]]; then
+        touch lock.tmp
+        cp -ar /var/www/moodle/. /var/www/html
+        mv /var/www/html/config-dist.php /var/www/html/config.php
+        rm lock.tmp
+        sed -i "/->dbtype/c\$CFG->dbtype    = 'mysqli';" /var/www/html/config.php
+        sed -i "/->dbhost/c\$CFG->dbhost    = 'db';" /var/www/html/config.php
+        sed -i "/->dbname/c\$CFG->dbname    = '"$ADMIN_USERNAME"';" /var/www/html/config.php
+        sed -i "/->dbuser/c\$CFG->dbuser    = '"$ADMIN_USERNAME"';" /var/www/html/config.php
+        sed -i "/->dbpass/c\$CFG->dbpass    = '"$ADMIN_PASSWORD"';" /var/www/html/config.php
+        sed -i "/->prefix/c\$CFG->prefix    = 'mdl_';" /var/www/html/config.php
+        sed -i "/->wwwroot/c\$CFG->wwwroot   = 'http://"$VIRTUAL_HOST"';" /var/www/html/config.php
+        sed -i "/->dataroot/c\$CFG->dataroot  = '/var/www/moodledata';" /var/www/html/config.php
+        sed -i "/->directorypermissions/c\$CFG->directorypermissions = "02777";" /var/www/html/config.php
+        sed -i "/->admin/c\$CFG->admin = 'admin';" /var/www/html/config.php
+        chown -R www-data:www-data /var/www/html
+        chmod -R 0755 /var/www/html
+        find /var/www/html -type f -exec chmod 0644 {} \;
+        chmod -R 0777 /var/www/moodledata
+        echo "Check DB!"
+        maxtry=0
+        while ! mysqladmin ping -h db -u ${ADMIN_USERNAME} -p${ADMIN_PASSWORD} 1>/dev/null 2>&1; do
+            if [[ $maxtry -le 12 ]]; then
+                echo "Wait ..."
+                sleep 5
+                maxtry=$(($maxtry + 1))
+            else
+                echo "Error: Database server is unreachable after 60 seconds!"
+                exit
+            fi
+        done
+        echo "DB ready!"
+        cd /var/www/html && \
+        php /var/www/html/admin/cli/install_database.php --lang=fr --adminuser=$ADMIN_USERNAME --adminpass=$ADMIN_PASSWORD --adminemail=$ADMIN_EMAIL --agree-license --fullname=$VIRTUAL_HOST --shortname=MOODLE3.8
     fi
 }
-moodle_cleanup_install_files(){
-    rm -r /web_data/moodle
-    echo "[OK] MOOLDE INSTALL FILES CLEANED UP"
-}
 if [[ "$1" == apache2* ]]; then
+    echo " "
+    echo " "
     logo_print
-    echo "[Initilizing ...]"
-    echo ""
+    echo " "
+    echo " "
     apache_set_servername
-    moodle_setup_files
-    moodle_update_config
-    moodle_securing_web
-    moodle_securing_data
-    moodle_setup_database
-    cron_service_start
-    echo ""
-    echo ""
-    echo "**** CONTAINER STARED SUCCESSFULY ****"
-    echo "below there will be the instant apache access and error log"
-    echo ""
-    echo ""
+    moodle_install
+    service cron start 1>/dev/null 2>&1;
+    echo " "
+    echo " "
+    echo "**** MOODLE CONTAINER STARED SUCCESSFULY ****"
+    echo "Notice: You website URL http://$VIRTUAL_HOST/"
+    echo "Notice: PhpMyAdmin is available under http://$VIRTUAL_HOST/phpmyadmin"
+    echo "Notice: Filemanager is available under http://$VIRTUAL_HOST/filemanage"
+    echo "Notice: below there will be the instant apache access and error log"
+    echo " "
+    echo " "
 fi
 exec "$@"
